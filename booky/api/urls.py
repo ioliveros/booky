372c380a73f4f9df95ae4cae487cdb9913cf7544
ioliveros.dev <ioliveros.dev@gmail.com>
2024-08-17 03:26:32 +0800

    ref: remove unused routes

--- booky/api/urls.py
+++ booky/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AuthorViewSet, BookViewSet, UserProfileViewSet, RegisterView
-# AddFavoriteBookView, RemoveFavoriteBookView
 
 router = DefaultRouter()
 router.register(r'authors', AuthorViewSet)
@@ -11,6 +10,4 @@ router.register(r'profile', UserProfileViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('register', RegisterView.as_view(), name='register'),
-    # path('favorites/add/', AddFavoriteBookView.as_view(), name='add-favorite-book'),
-    # path('favorites/remove/', RemoveFavoriteBookView.as_view(), name='remove-favorite-book'),
 ]
