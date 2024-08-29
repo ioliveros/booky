0e4e491d234a2c364c14a49e96b400cc94f5fff2
Ian Oliveros <ianoliveros@Ians-Mac-mini.local>
2024-08-30 02:25:09 +0800

    update

--- reco/app.py
+++ reco/app.py
@@ -6,22 +6,22 @@ from functools import lru_cache
 
 app = Flask(__name__)
 
-books_df = pd.read_json('dataset/books.json', orient='records', lines=True)
+books_df = pd.read_json('dataset/df_books_1m.json', orient='records', lines=True)
 
-with open('models/mlb.pkl', 'rb') as f:
+with open('models/mlb_1m.pkl', 'rb') as f:
     mlb = pickle.load(f)
 
-with open('models/knn_model.pkl', 'rb') as f:
+with open('models/knn_model_1m.pkl', 'rb') as f:
     knn = pickle.load(f)
 
-# @lru_cache(maxsize=128)
-def create_query_vector(genres):
+def create_query_vector(genres, num_features):
     query_genre_features = mlb.transform([genres])
-    query_vector = np.hstack([query_genre_features, np.array([[0]])])
+    query_vector = np.hstack([query_genre_features, np.zeros((1, num_features - query_genre_features.shape[1]))])
     return query_vector
 
 @app.route('/suggested_books', methods=['POST'])
 def recommend():
+
     data = request.json
     query_genres = data.get('genres', [])
     query_title = data.get('title', None)
@@ -29,7 +29,9 @@ def recommend():
     if not query_genres:
         return jsonify({'error': 'No genres provided'}), 400
     
-    query_vector = create_query_vector(query_genres)
+    num_features = knn.n_features_in_
+
+    query_vector = create_query_vector(query_genres, num_features)
     distances, indices = knn.kneighbors(query_vector)
     recommended_books = books_df.iloc[indices[0]]
     
@@ -38,8 +40,10 @@ def recommend():
 
     result = recommended_books[
         [
-            'title', 'average_rating', 'author_name', 
-            'description', 'original_publication_date', 
+            'title', 
+            'rating', 
+            'author_name',
+            'genre', 
          ]
     ].to_dict(orient='records')
     return jsonify(result)
